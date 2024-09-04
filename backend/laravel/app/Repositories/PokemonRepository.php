b9652178fa7e46c37fca0cbec7d06342262d921b
Douglas <dodosobreira@gmail.com>
2024-09-04 16:55:01 -0300

    FEAT: Add asynchronous request pooling for fetching all Pokemon

--- backend/laravel/app/Repositories/PokemonRepository.php
+++ backend/laravel/app/Repositories/PokemonRepository.php
@@ -2,32 +2,57 @@
 
 namespace App\Repositories;
 
-use App\Models\Pokemon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Http; 
+use App\Models\Pokemon;
 
 class PokemonRepository 
 {
     public function fetchAndStorePokemons()
     {
+        $client = new Client();
+        $nextPage = 'https://pokeapi.co/api/v2/pokemon?limit=20';  
+        $concurrency = 5;  
     
-        $nextPage = 'https://pokeapi.co/api/v2/pokemon';
+        do {
+            $response = Http::get($nextPage);
+            $data = $response->json();
+            $pokemons = $data['results'];
+            $nextPage = $data['next'];  
+    
+            $requests = function ($pokemons) {
+                foreach ($pokemons as $pokemon) {
+                    yield new Request('GET', $pokemon['url']);
+                }
+            };
+    
+            $pool = new Pool($client, $requests($pokemons), [
+                'concurrency' => $concurrency,
+                'fulfilled' => function ($response, $index) use ($pokemons) {
 
-        $response = Http::get($nextPage, ['limit' => 500]);
-
-        $pokemons = $response->json()['results'];
-
-        foreach ($pokemons as $pokemon) { 
-            $details = Http::get($pokemon['url'])->json();
-            
-            Pokemon::updateOrCreate(
-                ['name' => $details['name']],
-                [
-                'type' => $details['types'][0]['type']['name'],
-                'height_cm' => $this->convertHeightToCm($details['height']),
-                'weight_kg' => $this->convertWeightToKg($details['weight']),
-                ]
-            );
-        }
+                    $details = json_decode($response->getBody()->getContents(), true);
+    
+                    Pokemon::updateOrCreate(
+                        ['name' => $details['name']],
+                        [
+                            'type' => $details['types'][0]['type']['name'],
+                            'height_cm' => $this->convertHeightToCm($details['height']),
+                            'weight_kg' => $this->convertWeightToKg($details['weight']),
+                        ]
+                    );
+                },
+                'rejected' => function (RequestException $reason, $index) {
+                    echo "Erro ao buscar Pokémon: " . $reason->getMessage();
+                },
+            ]);
+    
+            $promise = $pool->promise();
+            $promise->wait();
+    
+        } while (!is_null($nextPage)); 
     }
 
     public function getAll($filter = null, $perPage = 15) 
